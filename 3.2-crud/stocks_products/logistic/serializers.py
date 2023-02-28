92991e96f9eee277f851e49f0e45b55b278831d8
Ivanov Nikita <destira@mail.ru>
2023-02-28 18:07:24 +0300

    Initial commit

---
+++ 3.2-crud/stocks_products/logistic/serializers.py
@@ -0,0 +1,66 @@
+from rest_framework import serializers
+from models import StockProduct, Product, Stock
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    # настройте сериализатор для продукта
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'description']
+
+
+
+class ProductPositionSerializer(serializers.ModelSerializer):
+    # настройте сериализатор для позиции продукта на складе
+    class Meta:
+        model = StockProduct
+        fields = ['id', 'stock', 'product', 'quantity', 'price']
+
+class StockSerializer(serializers.ModelSerializer):
+    positions = ProductPositionSerializer(many=True)
+
+    # настройте сериализатор для склада
+    class Meta:
+        model = Stock
+        fields = ['id', 'address', 'products']
+
+    def create(self, validated_data):
+        # достаем связанные данные для других таблиц
+        positions = validated_data.pop('positions')
+
+        # создаем склад по его параметрам
+        stock = super().create(validated_data)
+
+        # здесь вам надо заполнить связанные таблицы
+        # в нашем случае: таблицу StockProduct
+        # с помощью списка positions
+        for elem in positions:
+            stc_product = StockProduct.objects.create(
+                stock=stock,
+                product=elem['product'],
+                quantity=elem['quantity'],
+                price=elem['price'],
+            )
+            stc_product.save()
+        return stock
+
+    def update(self, instance, validated_data):
+        # достаем связанные данные для других таблиц
+        positions = validated_data.pop('positions')
+
+        # обновляем склад по его параметрам
+        stock = super().update(instance, validated_data)
+
+        # здесь вам надо обновить связанные таблицы
+        # в нашем случае: таблицу StockProduct
+        # с помощью списка positions
+        for elem in positions:
+            stc_product = StockProduct.objects.update_or_create(
+                stock=stock,
+                product=elem['product'],
+                quantity=elem['quantity'],
+                price=elem['price'],
+            )
+            stc_product.save()
+
+        return stock
